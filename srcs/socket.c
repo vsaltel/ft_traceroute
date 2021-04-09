280733c1662eaf5d07cbbc4efd12316a2ce906ea
Valentin <saltel.valentin@gmail.com>
2021-04-09 19:32:25 +0200

    test

--- srcs/socket.c
+++ srcs/socket.c
@@ -16,7 +16,7 @@ int	set_socket(t_tr *tr)
 	setsockopt (sock, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
     timeout.tv_sec = 3;
     timeout.tv_usec = 0;
-	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
+	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
 	tr->ttl++;
 	setsockopt(sock, IPPROTO_IP, IP_TTL, &tr->ttl, sizeof(tr->ttl));
 	return (sock);
