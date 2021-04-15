617c1d3bacae82dfbbb2a759716ac151d264855f
Valentin <saltel.valentin@gmail.com>
2021-04-15 20:36:45 +0200

    df_bit

--- srcs/socket.c
+++ srcs/socket.c
@@ -4,19 +4,22 @@ int	set_socket(t_tr *tr)
 {
 	int	sock;
 	int	size;
+	int	on;
 	struct timeval timeout;
 
-	sock = socket(tr->pr.sasend->sa_family, SOCK_RAW, IPPROTO_ICMP);
+	sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sock < 0)
 	{
 		ft_dprintf(2, "ft_traceroute: fail to create socket\n");
 		return (-1);
 	}
 	size = 60 * 1024;
-	setsockopt (sock, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
-    timeout.tv_sec = 2;
+	on = 1;
+	setsoctopt(sock, IPPROTO_IP, IP_HDRINCL, &on, sizeof(int));
+	//setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+    timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
-	setsockopt(sock, IPPROTO_IP, IP_TTL, &tr->ttl, sizeof(tr->ttl));
+	//setsockopt(sock, IPPROTO_IP, IP_TTL, &tr->ttl, sizeof(tr->ttl));
 	return (sock);
 }
