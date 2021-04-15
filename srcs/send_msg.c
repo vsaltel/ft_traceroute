95c62403ac6fa27258d4ed961469d70a245795db
Valentin <saltel.valentin@gmail.com>
2021-04-15 17:45:26 +0200

    df_bit

--- srcs/send_msg.c
+++ srcs/send_msg.c
@@ -8,15 +8,15 @@ void	send_msg(void)
 	char		sendbuf[BUFSIZE];
 
 	pckt = (t_send_pckt *)sendbuf;
-	pckt->icmp->icmp_type = ICMP_ECHO;
-	pckt->icmp->icmp_code = 0;
-	pckt->icmp->icmp_id = g_tr.pid;
-	pckt->icmp->icmp_seq = g_tr.msg_sent;
-	ft_memset(icmp->icmp_data, 0xa5, g_tr.datalen);
-	gettimeofday((struct timeval *)pckt->icmp->icmp_data, NULL);
+	pckt->icmp.icmp_type = ICMP_ECHO;
+	pckt->icmp.icmp_code = 0;
+	pckt->icmp.icmp_id = g_tr.pid;
+	pckt->icmp.icmp_seq = g_tr.msg_sent;
+	ft_memset(pckt->icmp.icmp_data, 0xa5, g_tr.datalen);
+	gettimeofday((struct timeval *)pckt->icmp.icmp_data, NULL);
 	len = 8 + g_tr.datalen;
-	pckt->icmp->icmp_cksum = 0;
-	pckt->icmp->icmp_cksum = checksum((u_short *) pckt->icmp, len);
+	pckt->icmp.icmp_cksum = 0;
+	pckt->icmp.icmp_cksum = checksum((u_short *) pckt->icmp, len);
 	gettimeofday(&g_tr.bef, NULL);
 	ret = sendto(g_tr.sockfd, sendbuf, len, 0,
 			g_tr.pr.sasend, g_tr.pr.salen);
