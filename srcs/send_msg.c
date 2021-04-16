bf66273849f1bc7c8514cf554492b446aa236617
Valentin <saltel.valentin@gmail.com>
2021-04-16 15:34:09 +0200

    df_bit

--- srcs/send_msg.c
+++ srcs/send_msg.c
@@ -50,7 +50,7 @@ void	send_msg(void)
 	pckt->icmp.icmp_cksum = 0;
 	pckt->icmp.icmp_cksum = checksum((u_short *) &pckt->icmp, sizeof(struct icmp) + g_tr.datalen);
 
-	ft_bzero(pckt->msg, BUFSIZE - 1);
+	ft_bzero(pckt->msg, sizeof(t_send_pckt) - BUFSIZE);
 	int	i;
 	i = -1;
 	while (++i < g_tr.datalen)
