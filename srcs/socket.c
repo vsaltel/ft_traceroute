051d58679dbffba0a054f99ffc03cf126f9b7204
Valentin <saltel.valentin@gmail.com>
2021-04-16 14:51:12 +0200

    df_bit

--- srcs/socket.c
+++ srcs/socket.c
@@ -20,6 +20,6 @@ int	set_socket(t_tr *tr)
     timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
-	setsockopt(sock, IPPROTO_IP, IP_TTL, &tr->ttl, sizeof(tr->ttl));
+	//setsockopt(sock, IPPROTO_IP, IP_TTL, &tr->ttl, sizeof(tr->ttl));
 	return (sock);
 }
